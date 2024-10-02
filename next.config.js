5e82001aa47ff7d3e4911e16c30bbcd435a9eb1f
Christian Ipanaque <christian.ipanaque@engineer.com>
2024-10-01 18:31:16 -0700

    fix css

--- next.config.js
+++ next.config.js
@@ -3,16 +3,14 @@
 const nextConfig = {
   output: "export",
   distDir: "docs",
-  // Ensure that Next.js knows this is a static site
   trailingSlash: true,
-  // Disable image optimization for static sites
   images: {
     unoptimized: true,
   },
-  // Optional: Add basePath if your site will be hosted in a subdirectory
-  // basePath: '/your-base-path',
-
-  // Add any other configuration options here
+  assetPrefix: ".", // This tells Next.js to use relative paths for assets
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 module.exports = nextConfig;
