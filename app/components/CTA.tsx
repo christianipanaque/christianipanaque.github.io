930b61712a9f523112096c3f6dfec3b3f8e00e75
Christian Ipanaque <christian.ipanaque@engineer.com>
2024-11-27 03:26:17 -0500

    update cta

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -1,50 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function ExternalRedirectLink({ primaryText, secondaryText }) {
-  useEffect(() => {
-    const handleExternalRedirect = (event: MouseEvent) => {
-      const targetUrl = "https://wa.me/message/GGSWDFMMNE3HP1"; // Replace with your actual URL
-
-      const isTikTokBrowser = (() => {
-        if ("userAgentData" in navigator) {
-          const userAgentData = navigator.userAgentData as any;
-          return userAgentData.brands.some((brand: { brand: string }) =>
-            brand.brand.includes("TikTok")
-          );
-        }
-        return navigator.userAgent.includes("TikTok");
-      })();
-
-      if (isTikTokBrowser) {
-        event.preventDefault(); // Prevent default behavior of <a> tag
-
-        // Redirect to external browser
-        window.location.href = `https://www.openinbrowser.com?url=${encodeURIComponent(
-          targetUrl
-        )}`;
-      }
-    };
-
-    // Attach the event to the link
-    const linkElement = document.getElementById("cta-link-id"); // Replace with your <a> tag ID
-    if (linkElement) {
-      linkElement.addEventListener("click", handleExternalRedirect);
-    }
-
-    // Cleanup event listener on unmount
-    return () => {
-      if (linkElement) {
-        linkElement.removeEventListener("click", handleExternalRedirect);
-      }
-    };
-  }, []);
   return (
     <a
       id="cta-link-id"
       className="button-custom"
-      href="https://wa.me/message/GGSWDFMMNE3HP1"
+      href="https://buy.stripe.com/8wM8y9eyRehl7fi002"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Open the link in a new browser tab"
