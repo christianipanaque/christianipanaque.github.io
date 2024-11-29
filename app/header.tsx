daac400c8ff6238887a378428bbc2157c8a5fcef
Christian Ipanaque <christian.ipanaque@engineer.com>
2024-11-29 09:25:01 -0500

    html only start fresh, again, but its okay

--- app/header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header>
-      <h1>El Poder de sus Palabras</h1>
-    </header>
-  );
-}
--- posts/index.md
+++
@@ -1,5 +0,0 @@
----
-title: "My First Post"
-date: "2024-11-24"
----
-This is the content of the markdown file.
